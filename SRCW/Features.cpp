ad891dd93e66442702159b4b834a514399261455
HAWGT <r4th4ck3r@gmail.com>
2025-10-05 04:46:58 +0100

    Init

---
+++ SRCW/Features.cpp
@@ -0,0 +1,78 @@
+#include "Features.h"
+
+void HookGame()
+{
+	//TODO: 
+	//enable console
+
+	bool bHooked = false;
+
+	while (!bHooked)
+	{
+		auto World = SDK::UWorld::GetWorld();
+		auto Engine = SDK::UEngine::GetEngine();
+
+		if (!World || !Engine || !Engine->GameViewport)
+		{
+			continue;
+		}
+
+		Orig_AActor_ProcessEvent = reinterpret_cast<AActor_ProcessEvent_t>(TrampHook64(PatternScan("48 89 5C 24 10 48 89 6C 24 18 57 48 83 EC ? F7 82 B0 00 00 00 ? ? ? ?"), (BYTE*)hk_AActor_ProcessEvent, 15));
+
+		bHooked = true;
+	}
+}
+
+void UnlockAll()
+{
+	for (int i = 0; i < SDK::UObject::GObjects->Num(); i++)
+	{
+		SDK::UObject* Obj = SDK::UObject::GObjects->GetByIndex(i);
+
+		if (!Obj)
+			continue;
+
+		if (Obj->IsDefaultObject())
+			continue;
+
+		if (Obj->IsA(SDK::UHonorTitleListDataAsset::StaticClass()))
+		{
+			SDK::UHonorTitleListDataAsset* Title = static_cast<SDK::UHonorTitleListDataAsset*>(Obj);
+
+			for (const auto& t : Title->HonorTitleTableDataMap)
+			{
+				std::cout << t.Key() << "\n" << std::endl;
+				SDK::UAppSaveGameHelper::UnlockHonorTitle(t.Key());
+			}
+
+			bUnlocked = true;
+		}
+	}
+
+	for (int i = 0; i < (uint8_t)SDK::EDriverId::Num; i++)
+	{
+		SDK::UAppSaveGameHelper::SetDriverSelectable(SDK::EDriverId(i));
+	}
+	
+	SDK::UMachineCustomizeUtilityLibrary::StoreAllAura();
+	SDK::UMachineCustomizeUtilityLibrary::StoreAllHorn();
+	SDK::UMachineCustomizeUtilityLibrary::StoreAllMachineAssembly();
+	SDK::UMachineCustomizeUtilityLibrary::StoreAllMachineParts();
+	SDK::UMachineCustomizeUtilityLibrary::StoreAllSticker();
+	SDK::UMachineCustomizeUtilityLibrary::UnlockGadgetAll();
+
+	std::cout << "Unlocked Everything!\n";
+}
+
+void __fastcall hk_AActor_ProcessEvent(SDK::AActor* Class, SDK::UFunction* Function, void* Parms)
+{
+	if (!Function->GetName().compare("ReceiveDrawHUD") && !bUnlocked)
+	{
+		UnlockAll();
+	}
+
+	//std::cout << Function->GetName() << "\n";
+
+	Orig_AActor_ProcessEvent(Class, Function, Parms);
+}
+
---
+++ SRCW/Features.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <random>
+
+#include "Helpers.h"
+#include "CppSDK/SDK/Engine_classes.hpp"
+#include "CppSDK/SDK/BP_UnionGameInstance_classes.hpp"
+#include "CppSDK/SDK/BP_MenuPlayerController_classes.hpp"
+#include "CppSDK/SDK/UnionSystem_classes.hpp"
+#include "CppSDK/SDK/UnionSystem_structs.hpp"
+#include "CppSDK/SDK/UNION_parameters.hpp"
+#include "CppSDK/SDK/BP_MenuRacerLobby_classes.hpp"
+
+inline const char ConfigFileName[] = ".\\UNION\\Binaries\\Win64\\SRCW.ini";
+
+inline bool bUnlocked = false;
+
+void HookGame();
+void UnlockAll();
+
+void __fastcall hk_AActor_ProcessEvent(SDK::AActor* Class, SDK::UFunction* Function, void* Parms);
+typedef void(__fastcall* AActor_ProcessEvent_t)(SDK::AActor* Class, SDK::UFunction* Function, void* Parms);
+inline AActor_ProcessEvent_t Orig_AActor_ProcessEvent;
+
